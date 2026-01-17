ad6fd82152663445198cbeef4a71e82566b8b0a1
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-01-17 00:38:45 -0200

    Update working directory

--- Frontend-Projects/Age-Calculator/script.js
+++ Frontend-Projects/Age-Calculator/script.js
@@ -1,47 +1,51 @@
-const dateInput = document.getElementById('date-picker');
-const calculateBtn = document.getElementById('calculate-btn');
-const result = document.getElementById('result');
+let userInput = document.getElementById("date");
+let result = document.getElementById("result");
+userInput.max = new Date().toISOString().split("T")[0];
 
-calculateBtn.addEventListener("click", function() {
-  const [y, m, d] = dateInput.value.split('-');
-  const birthDate = new Date(y,m-1,d)
-  const today = new Date();
-
-  if (dateInput.value === "") {
-    alert("Please enter your birth date!");
+function calculateAge() {   
+  if (!userInput.value) {
+    result.style.color = "red";
+    result.innerHTML = "Please select a date!"
     return;
   }
+  const birthDate = new Date(userInput.value);
 
-  let y1 = birthDate.getFullYear();
-  let m1 = birthDate.getMonth() + 1;
   let d1 = birthDate.getDate();
+  let m1 = birthDate.getMonth() + 1;
+  let y1 = birthDate.getFullYear();
 
-  let y2 = today.getFullYear();
-  let m2 = today.getMonth() + 1;
+  let today = new Date();
+
   let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
 
-  let y3, m3, d3;
+  let d3, m3, y3;
 
-  if (d2 >= d1) {
-    d3 = d2 - d1;
-  } else {
-    m2--;
-    d3 = getDaysInMonth(y1, m1) + d2 - d1;
-  }
+  y3 = y2 - y1;
 
   if (m2 >= m1) {
     m3 = m2 - m1;
   } else {
-    y2--;
+    y3--;
     m3 = 12 + m2 - m1;
   }
 
-  y3 = y2 - y1;
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = getDateInMonth(y2, m2 - 1) + d2 - d1;
+  }
 
-  result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span> days old.`;
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
 
-})
+  result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span> days old.`
+}
 
-function getDaysInMonth(year, month) {
+function getDateInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
