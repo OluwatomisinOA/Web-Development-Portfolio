b32996055cb0311b838ff882e646d63ed2667cd1
Akinsola Oluwatomisin Oswald <akinsolaoluwatomisin475@gmail.com>
2026-02-05 18:05:27 +0100

    Update working directory

---
+++ invoicify/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-slate-900 text-white min-h-screen flex items-center justify-center text-3xl font-bold">
+      Hello Command Center
+    </div>
+  )
+}
+
+export default App
