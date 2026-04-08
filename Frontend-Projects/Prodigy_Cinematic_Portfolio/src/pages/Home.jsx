58775c8e5587d296a0aaa3de89ac87547c3b65d6
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-08 23:34:21 +0100

    Update Hone.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Home.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import Hero from "../components/home/Hero";
+import SignatureWork from "../components/home/SignatureWork";
 
 export default function Home() {
     return (
+       <>
         <Hero />
+        <SignatureWork />
+       </>
     )
 }
