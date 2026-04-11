dca617a4bf8a1e1e206ec489122527ba4aaf5bde
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-11 22:15:58 +0100

    Update Portfolio.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Portfolio.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Portfolio.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import { projects } from "../data/projects";
+import FilterBar from "../components/portfolio/FilterBar";
+import MasonryGrid from "../components/portfolio/MasonryGrid";
+
 export default function Portfolio() {
-    return <div>Portfolio Page</div>;
+
+    const [activeCategory, setActiveCategory] = useState('all');
+    const filteredProjects = projects.filter(project => (
+        activeCategory === 'all' || project.category === activeCategory
+    ));
+
+    return (
+        <>
+            <FilterBar 
+                activeCategory={activeCategory}
+                onCategoryChange={setActiveCategory}
+            />
+            <MasonryGrid 
+                projects={filteredProjects}
+            />
+        </>
+    )
 }
