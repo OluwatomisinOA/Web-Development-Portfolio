d2cd1368dc75420163b5b434b0c08f9b49c1ea27
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-11 09:23:03 +0100

    Update Home.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Home.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import Hero from "../components/home/Hero";
 import SignatureWork from "../components/home/SignatureWork";
 import AboutTeaser from "../components/home/AboutTeaser";
+import ServicesOverview from "../components/home/ServicesOverview";
+import Stats from "../components/home/Stats";
+import CTABanner from "../components/home/CTABanner";
 
 export default function Home() {
     return (
@@ -8,6 +11,9 @@ export default function Home() {
         <Hero />
         <SignatureWork />
         <AboutTeaser />
+        <ServicesOverview />
+        <Stats />
+        <CTABanner />
        </>
     )
 }
