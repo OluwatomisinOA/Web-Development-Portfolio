8e8f027e95f21b544ec9a3b1b2a7761256aed447
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-12 23:35:12 +0100

    Update Portfolio.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Portfolio.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/pages/Portfolio.jsx
@@ -6,9 +6,31 @@ import MasonryGrid from "../components/portfolio/MasonryGrid";
 export default function Portfolio() {
 
     const [activeCategory, setActiveCategory] = useState('all');
+    const [lightBoxIndex, setLightBoxIndex] = useState(null);
+
     const filteredProjects = projects.filter(project => (
         activeCategory === 'all' || project.category === activeCategory
     ));
+
+    const handleOpen = (index) => (
+        setLightBoxIndex(index)
+    )
+
+    const handleClose = () => (
+        setLightBoxIndex(null)
+    )
+
+    const handleNext = () => (
+        setLightBoxIndex((lightBoxIndex) => lightBoxIndex + 1)
+    )
+
+    const handlePrev = () => (
+        if (lightBoxIndex > 0) {
+            setLightBoxIndex((lightBoxIndex) => lightBoxIndex - 1)
+        } else {
+            setLightBoxIndex(null)
+        }
+    )
 
     return (
         <>
