d047585624dc60645a095414a2d60e1656693630
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-13 02:09:04 +0100

    Update MasonryGrid.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/portfolio/MasonryGrid.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/portfolio/MasonryGrid.jsx
@@ -1,13 +1,16 @@
+import { AnimatePresence } from "framer-motion";
 import GalleryCard from "./GalleryCard";
 
-export default function MasonryGrid({projects}) {
+export default function MasonryGrid({projects, onOpen}) {
     return (
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
-            {projects.map((project) => (
-                <div key={project.id} className="break-inside-avoid mb-4">
-                    <GalleryCard project={project}/>
-                </div>
-            ))}
+            <AnimatePresence>
+                {projects.map((project, index) => (
+                    <div key={project.id} className="break-inside-avoid mb-4">
+                        <GalleryCard project={project} onOpen={onOpen} index={index}/>
+                    </div>
+                ))}
+            </AnimatePresence>
         </div>
     )
 }
