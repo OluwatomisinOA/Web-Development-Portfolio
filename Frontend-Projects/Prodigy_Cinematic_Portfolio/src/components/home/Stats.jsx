c57a2ef322e49aca6b469c2bf291e277962488bd
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-22 08:35:05 +0100

    Update Stats.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/home/Stats.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/home/Stats.jsx
@@ -34,6 +34,7 @@ export default function Stats() {
                         key={index}
                         initial={{opacity: 0, y: 20}}
                         whileInView={{opacity: 1, y: 0}}
+                        viewport={{once: true}}
                         transition={{duration: 0.8, delay: 0.5 + index * 0.1}}
                         className="flex flex-col"
                     >
