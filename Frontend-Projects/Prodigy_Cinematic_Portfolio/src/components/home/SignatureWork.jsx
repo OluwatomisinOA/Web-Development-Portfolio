0db74f5e3a0f6ea5fba2c0f161f0bc139880d1de
StarTech <akinsolaoluwatomisin475@gmail.com>
2026-04-24 10:52:39 +0100

    Update SignatureWork.jsx

--- Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/home/SignatureWork.jsx
+++ Frontend-Projects/Prodigy_Cinematic_Portfolio/src/components/home/SignatureWork.jsx
@@ -1,3 +1,4 @@
+import { img } from "framer-motion/client";
 import { projects } from "../../data/projects";
 import { motion } from "framer-motion";
 
@@ -21,7 +22,7 @@ export default function SignatureWork() {
                         viewport={{once: true}}
                         transition={{duration: 0.8, delay: 0.3 + index * 0.1}}
                     >
-                        <img src={project.image} className="w-full h-80 object-cover"/>
+                        {project.image.endsWith('.mp4') ? <video src={project.image} className="w-full h-80 object-cover" autoPlay muted loop playsInline /> : <img src={project.image} className="w-full h-80 object-cover" />}
                         <div className="opacity-0 group-hover:opacity-100 absolute inset-0 transition-opacity duration-500 bg-gradient-to-t from-[#080808]/90 to-transparent"></div>
                         <div className="absolute bottom-0 left-0 p-6 flex flex-col gap-1">
                             <p className="text-[#C9A84C] font-mono text-sm translate-y-4 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-500">
